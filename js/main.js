7b4b1603afa996167aad0da4bfaf9d5e0a3f7672
mostafaelhawary1987 <hoohoo1987@gmail.com>
2025-04-08 21:48:23 +0200

    create-about-page

--- js/main.js
+++ js/main.js
@@ -27,6 +27,28 @@ $(document).ready(function () {
         }
         $(".faq-title").not(this).siblings().slideUp(500);
     })
+        ///////// landing header /////////
+    $('a[data-scroll^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = $(this).attr("data-scroll"),
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 100
+        }, 500, 'swing');
+        if ($(window).width() < 992) {
+            $("nav").toggleClass("active");
+            $("body").toggleClass("overflow");
+            $(".menu-overlay").fadeToggle(400);
+        }
+    });
     
 
 });
